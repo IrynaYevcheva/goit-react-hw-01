df69744dd76eb3e3a58571ff9cd8cc2b9b0a01cd
Iryna Yevcheva <irenmikos@gmail.com>
2024-02-11 16:29:14 +0200

    update import json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import { Profile } from "./profile/Profile";
-import { FriendList } from "./friendList/FriendList";
-import { TransactionHistory } from "./transactionHistory/TransactionHistory";
+import { Profile } from "../profile/Profile";
+import { FriendList } from "../friendList/FriendList";
+import { TransactionHistory } from "../transactionHistory/TransactionHistory";
 
-import userData from "./profile/userdata.json";
-import friends from "./friendList/friends.json";
-import transactions from "./transactionHistory/transactions.json";
+import userData from "../../data/userdata.json";
+import friends from "../../data/friends.json";
+import transactions from "../../data/transactions.json";
 
 export default function App() {
   const { username, tag, location, avatar, stats } = userData;
